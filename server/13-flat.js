7c99cacbcd65b628423affc242ebd52e9f6b1fae
andrescaro16 <julicaro2003@gmail.com>
2023-03-24 10:45:41 -0500

    Concat, Flat y FlatMap

---
+++ server/13-flat.js
@@ -0,0 +1,22 @@
+const matriz = [
+    [1,2,3],
+    [4,5,6, [1,2, [1,2]]],
+    [7,8,9]
+];
+
+// Traditional
+const newArray = [];
+for (let i = 0; i < matriz.length; i++) {
+    const array = matriz[i];
+    for (let j = 0; j < array.length; j++) {
+        const element = matriz[i][j];
+        newArray.push(element);
+    }
+}
+
+// Flat
+const rta = matriz.flat(3);
+
+// Tests
+console.log('for', newArray);
+console.log('flat', rta);
---
+++ server/14-flatMap.js
@@ -0,0 +1,49 @@
+const users = [
+  { userId: 1, username: "Tom", attributes: ["Nice", "Cute"] },
+  { userId: 2, username: "Mike", attributes: ["Lovely"] },
+  { userId: 3, username: "Nico", attributes: ["Nice", "Cool"] },
+];
+
+// Map y Flat
+const rta = users.map((user) => user.attributes).flat();
+
+// FlatMap
+const rta2 = users.flatMap((user) => user.attributes);
+
+// Tests
+console.log("map-flat", rta);
+console.log("rta2", rta2);
+
+
+// Exercise / Example with google calendar api
+const calendars = {
+  primaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 15),
+      endDate: new Date(2021, 1, 1, 15, 30),
+      title: "Cita 1",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 17),
+      endDate: new Date(2021, 1, 1, 18),
+      title: "Cita 2",
+    },
+  ],
+  secondaryCalendar: [
+    {
+      startDate: new Date(2021, 1, 1, 12),
+      endDate: new Date(2021, 1, 1, 12, 30),
+      title: "Cita 2",
+    },
+    {
+      startDate: new Date(2021, 1, 1, 9),
+      endDate: new Date(2021, 1, 1, 10),
+      title: "Cita 4",
+    },
+  ],
+};
+
+const rta3 = Object.values(calendars).flatMap(item => {
+    return item.map(date => date.startDate);
+});
+console.log(rta3);
